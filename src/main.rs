dcb007340983164b58cb8792698d16cdd980fb33
data <K1NGKAOS100@Gmail.com>
2024-11-25 01:21:24 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,17 @@ fn main() {
         print!("\x1B[2J\x1B[1;1H"); // clear screen
         println!("{}d:{}h:{}m:{}s ({:?})", days, hour, minute, second, args[1]);
         second = second + 1;
-        if second > 59 { // if greater than 59
+        if second > 59 {
             second = 0;
+            minute = minute + 1;
             if minute > 59 {
                 minute = 0;
+                hour = hour + 1;
                 if hour > 23 {
-                	hour = 0;
-                	days = days + 1;
+                    hour = 0;
+                    days = days + 1;
                 }
-                else { hour = hour + 1;}
             }
-            else { minute = minute + 1; }
         }
         thread::sleep(time::Duration::from_secs(1));
     }
